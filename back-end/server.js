3916d785fc32fcb2eed149851afdd2d018f6301e
Soroush Rasti <rasti.soroush@gmail.com>
2020-09-18 10:51:18 +0200

    express back-_ens signin

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,41 @@
 const express = require('express');
-
+const bodyparser=require('body-parser');
 
 const app=express();
+app.use(express.json());
 
+const database={
+  users:[
+      {
+          id:"123",
+          name:"john",
+          email:"john@email.com",
+          password:"cookies",
+          entries:0,
+          joined: new Date()
+      },
+      {
+        id:"125",
+        name:"sally",
+        email:"sally@email.com",
+        password:"12345",
+        entries:0,
+        joined: new Date()
+    }
+  ]
+}
 
 app.get('/',(req,res)=>{
     res.send('it is working')
 })
 
-app.listen(3000,console.log('app is running'))
+app.post('/signin',(req,res)=>{
+    if (req.body.password===database.users[0].password && req.body.email===database.users[0].email){
+        res.json('success')}else{
+            res.status(400).json('error logging')
+        }
+})
+
+
+app.listen(3000,()=>{
+console.log('app is running')})
