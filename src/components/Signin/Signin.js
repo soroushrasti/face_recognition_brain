81ee6c766e5048a21a686b1d3b40f0eb2092b690
Soroush Rasti <rasti.soroush@gmail.com>
2020-10-15 13:46:22 +0330

    first page font one column

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -43,13 +43,18 @@ class Signin extends React.Component {
     <div>
 
       <article className="br3 ba b--black-10 mv4 w-100 w-20-m w-105-l mw8 shadow-1 center f4  pa3 fw5 lh-copy dim black db ">
-        <p className="lh-copy measure ">
+      <div> <p >
           Welcome to Soroush Rasti's Profelio website!</p>
+          </div>
+          <div>
         <p >It is employed by HTML/CSS, Asynchronous JavaScript, React.js, 
         RESTful API, in front-end, to recognize a face 
         in your picture using the Clarifai Model.</p>
+        </div>
+        <div>
         <p >Node.js, Express.js, PostgresSQL and Bcrypt are implemented in
          the back-end to handle the registration precess securely.</p>
+         </div>
 
       </article>
 
